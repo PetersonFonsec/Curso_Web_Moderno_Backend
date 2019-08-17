65bf30ba32e778469416793ea20c3f2cf883ec04
Peterson Fonseca <petersonsimiao1@gmail.com>
2019-08-17 03:33:45 -0300

    implemented passport in routes main routes

--- config/routes.js
+++ config/routes.js
@@ -8,38 +8,46 @@ module.exports = app => {
     app.post('/validateToken', validateToken )
 
     app.route('/users')
+        .all(app.config.auth.authenticate())
        .post(user.save)
        .get(user.get)
 
     app.route('/users/:id')
+        .all(app.config.auth.authenticate())
         .put(user.save)
         .get(user.getById)
     
     const { category } = app.api
     
     app.route('/categories')
+        .all(app.config.auth.authenticate())    
         .post(category.save)
         .get(category.get)
 
     app.route('/categories/tree')
+        .all(app.config.auth.authenticate())    
         .get(category.getTree)
 
     app.route('/categories/:id')
-         .get(category.getById)
-         .put(category.save)
-         .delete(category.remove)
+        .all(app.config.auth.authenticate())    
+        .get(category.getById)
+        .put(category.save)
+        .delete(category.remove)
 
     const { article } = app.api
 
     app.route('/articles')
+        .all(app.config.auth.authenticate())
         .get(article.get)
         .post(article.save)
 
     app.route('/articles/:id')
+        .all(app.config.auth.authenticate())
         .get(article.getById)
         .put(article.save)
         .delete(article.remove)
 
     app.route('/categories/:id/articles')
+        .all(app.config.auth.authenticate())
         .get(app.api.articles.getByCategory)
 }
